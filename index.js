c4072d2e76513694dbf26d5764f60e522914ec94
MAHMOD-HASAN <osthir7788@gmail.com>
2021-11-13 18:50:28 +0600

    second commit

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ async function run () {
         const database = client.db('WatchShop');
         const productCollection = database.collection('watches');
         const userCollection = database.collection('users');
+        const orderCollection = database.collection('orders');
+        const reviewCollection = database.collection('reviews');
+
+        // products post on database 
+        // products get and show in UI
 
         // products send database by post api
         app.post('/product', async (req, res) => {
@@ -44,16 +49,128 @@ async function run () {
             const product = await productCollection.findOne(query);
             res.json(product);
         })
+                // FINISH FIRST STEP
+//==================================================================================
 
 
-        // user related post and get api
-        // send user on database
+
+          
+        // set user information on database
+        // get user information and set to UI
+
+        // POST user information to database
         app.post('/user', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
             res.json(result);
         })
+
+        // GET all user from database
+        app.get('/alluser', async (req, res) => {
+            const user = userCollection.find({});
+            const result = await user.toArray();
+            res.send(result);
+        })
+
+        // (UPSERT) if user exist than update nor insert 
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const filter = {email : user.email};
+            const option = {upsert : true};
+            const update = {$set : user};
+            const result = await userCollection.updateOne(filter, update, option);
+            res.json(result);
+        })
+
+        // DELETE user with uniqe id
+        app.delete('/alluser/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // (UPDATE) make admin an user
+        app.put('/user/admin', async (req, res) => {
+            const user = req.body;
+            const filter = {email : user.email};
+            const updateDoc = {$set : {role : 'admin'}};
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+        // (GET) check the exist user - admin or not
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email : email};
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({admin : isAdmin})
+        })
+                 // FINISH SECOND STEP
+//===================================================================================
+
+
+
+
+        // user order send and get and show on UI
+
+        // (POST) send user order on database
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+
+        // (GET) get a perticuler user order from database
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = {email : email};
+            const order = orderCollection.find(query);         
+            const result = await order.toArray();
+            res.json(result);
+        })
+
+        // (DELETE) normal user can delete these order
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // (GET) get all order from database
+        app.get('/order/all', async (req, res) => {
+            const order = orderCollection.find({});
+            const result = await order.toArray();
+            res.send(result);
+        })
+
+        // (DELETE) admin can delete these order
+        app.delete('/order/all/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+                      // FINISH THIRE STEP
+//==================================================================================
+
+
+        // user review post on database
+        app.post('/review', async(req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.json(result);
+        })
+
     }
+
+
+
     finally {
         // await client.close();
     }
